792a59c3cbed4ae4f508bc91691f8ce514feb99c
tessychen <66759126+tessychen@users.noreply.github.com>
2020-07-28 17:21:39 +0800

    Add files via upload

---
+++ StartYourArg.js
@@ -0,0 +1,9 @@
+﻿function getText() {
+    //12document.getElementById('btn').style.backgroundColor="white";
+
+    var input_str = document.getElementById('ans').value;//取輸入的值;
+    //如果沒有詩入任何東西，跳出alert。如果有東西才新增留言;
+    if (input_str == "") {
+        alert("人生不能有空白這個答案");
+    }
+}
