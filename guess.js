5d30d3d642cbea264cf340e8e8e06fd403191d30
tessychen <66759126+tessychen@users.noreply.github.com>
2020-08-23 18:56:01 +0800

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,16 @@
+﻿function getText() {
+    //12document.getElementById('btn').style.backgroundColor="white";
+
+    var input_str = document.getElementById('ans').value;//取輸入的值;
+    //如果沒有詩入任何東西，跳出alert。如果有東西才新增留言;
+    
+    if (input_str == "DWARFS" || input_str == "dwarfs" || input_str == "Dwarfs") {
+        window.location.href = 'DWARFS.html';
+    }
+    else if (input_str == "QUEEN" || input_str == "queen" || input_str == "Queen") {
+        window.location.href = 'QUEEN.html';
+    }
+    else {
+        alert("找水哥");
+    }
+}
